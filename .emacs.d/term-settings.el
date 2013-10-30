9c9075defccbab59b82f64bcddc6fba4e9163523
Stuart Olsen <so1132@txstate.edu>
2013-10-30 13:46:35 -0500

    Patched term.el from 24.3.1

--- .emacs.d/term-settings.el
+++ .emacs.d/term-settings.el
@@ -8,36 +8,6 @@
 ;; (setq term-default-bg-color (frame-parameter nil 'background-color))
 ;; (setq term-default-fg-color (frame-parameter nil 'foreground-color))
 
-;; TODO: fix colors for emacs -nw
-(if (string< emacs-version "24.3.1")
-    (setq ansi-term-color-vector
-          [nil term-default-bg-color
-               "#F7977A"
-               "#3BB878"
-               "#FFF79A"
-               "#438CCA"
-               "#F06EA9"
-               "#00FFFF"
-               term-default-fg-color]) ; gnome-terminal colors
-  ;; (custom-set-faces `(term-color-black   ((t (:background ,term-default-bg-color :foreground ,term-default-bg-color))))
-  ;;                   '(term-color-red     ((t (:background "#F7977A"              :foreground "#F7977A"))))
-  ;;                   '(term-color-green   ((t (:background "#3BB878"              :foreground "#3BB878"))))
-  ;;                   '(term-color-yellow  ((t (:background "#FFF79A"              :foreground "#FFF79A"))))
-  ;;                   '(term-color-blue    ((t (:background "#438CCA"              :foreground "#438CCA"))))
-  ;;                   '(term-color-magenta ((t (:background "#F06EA9"              :foreground "#F06EA9"))))
-  ;;                   '(term-color-cyan    ((t (:background "#00FFFF"              :foreground "#00FFFF"))))
-  ;;                   `(term-color-white   ((t (:background ,term-default-fg-color :foreground ,term-default-fg-color))))))
-  )
-;; (setq ansi-term-color-vector
-;;       [nil "grey15"
-;;            "burlywood1"
-;;            "spring green"
-;;            "grey80"
-;;            "deep sky blue"
-;;            "#ff51b3"
-;;            "pale turquoise"
-;;            "white"])
-
 ;; Use  and  for output suppression
 (setq ansi-term-suppress-output nil)
 (make-local-variable 'ansi-term-suppress-output)
