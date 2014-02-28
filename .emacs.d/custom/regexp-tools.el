2ba8fa3bebcebe69a611d72d6697ffb21d560cc3
Stuart Olsen <so1132@txstate.edu>
2014-02-28 04:53:14 -0600

    Factored string processing out of collect-matches

--- .emacs.d/custom/regexp-tools.el
+++ .emacs.d/custom/regexp-tools.el
@@ -1,20 +1,22 @@
 ;;; regexp-tools.el --- Custom functions for manipulating text by regexp
 
+(defun collect-matches (regexp string)
+  (save-match-data
+    (let ((submatch-start 0)
+          (submatch-end   0))
+      (loop while (string-match regexp string submatch-end) collect
+            (progn
+              (setq submatch-start (match-beginning 0) submatch-end (match-end 0))
+              (substring string submatch-start submatch-end))))))
+
 (defun collect-matches-to-kill-ring (regexp &optional start end)
   "Collects matches of REGEXP into a list and pushes that list onto the kill ring"
   (interactive "MRegexp to match: \nr")
   (save-excursion
-    (save-match-data
-      (goto-char start)
-      (let* ((region-string  (buffer-substring-no-properties start end))
-             (submatch-start 0)
-             (submatch-end   0)
-             (matches
-              (loop while (string-match regexp region-string submatch-end) collect
-                    (progn
-                      (setq submatch-start (match-beginning 0) submatch-end (match-end 0))
-                      (substring region-string submatch-start submatch-end)))))
-        (kill-new (prin1-to-string matches))))))
+    (goto-char start)
+      (let* ((region-string (buffer-substring-no-properties start end))
+             (matches (collect-matches regexp region-string)))
+        (kill-new (prin1-to-string matches)))))
 
 (defun replace-symbol (old-symbol new-symbol &optional start end)
   "Performs the specified symbol replacement within the given region.
