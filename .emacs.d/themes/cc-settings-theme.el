497ed446acda790de9e428330da71a08b1e02097
Stuart Olsen <so1132@txstate.edu>
2014-02-28 04:54:06 -0600

    Configured auto-completion for clang

--- .emacs.d/themes/cc-settings-theme.el
+++ .emacs.d/themes/cc-settings-theme.el
@@ -18,15 +18,37 @@ should be an alist mapping mode-map names to mode-hook names.")
   (let ((binding-alist (make-alist cc-keybind-alist)))
     (dolist (map/hook cc-map/hook-alist)
       (let ((mode-map-sym (car map/hook))
-	    (mode-hook-sym (cdr map/hook)))
-	(add-hook mode-hook-sym (λ ()
-				  (dolist (binding binding-alist)
-				    (define-key (eval mode-map-sym)
-				      (read-kbd-macro (car binding))
-				      (cdr binding)))))))))
+            (mode-hook-sym (cdr map/hook)))
+        (add-hook mode-hook-sym (λ ()
+                                  (dolist (binding binding-alist)
+                                    (define-key (eval mode-map-sym)
+                                      (read-kbd-macro (car binding))
+                                      (cdr binding)))))))))
 
 (eval-after-load 'cc-mode
-    (hook-up-cc-keybinds))
+  (hook-up-cc-keybinds))
+
+
+;; Auto-complete settings
+
+(defvar *cc-mode-ac-sources*
+  '(ac-source-clang))
+
+(defvar *clang-get-driver-parameters-command*
+  "clang++ '-###' -x c++ - 2>&1")
+
+(defun clang-include-flags ()
+  (let* ((raw-output (shell-command-to-string *clang-get-driver-parameters-command*))
+         (paths (collect-matches "\"[^ ]*?include[^ ]*?\"" raw-output)))
+    (mapcar (λ (path)
+              (concat "-I" (substring path 1 (- (length path) 1))))
+            paths)))
+
+(add-hook 'c-mode-common-hook (λ ()
+                                (when (featurep 'auto-complete)
+                                  (require 'auto-complete-clang)
+                                  (setq ac-clang-flags (append (clang-include-flags) ac-clang-flags))
+                                  (setq ac-sources *cc-mode-ac-sources*))))
 
 
 
