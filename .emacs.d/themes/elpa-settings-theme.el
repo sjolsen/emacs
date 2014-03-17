5611ddfdd3e985b498b4931da827f2c22e20b236
Stuart Olsen <so1132@txstate.edu>
2014-03-17 14:24:15 -0500

    ELPA magic.

--- .emacs.d/themes/elpa-settings-theme.el
+++ .emacs.d/themes/elpa-settings-theme.el
@@ -1,11 +1,44 @@
 (deftheme elpa-settings
   "Settings for Emacs' package manager")
 
+(require 'package)
+(package-initialize)
+
+(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa"     . "http://melpa.milkbox.net/packages/")))
+
+(defvar *elpa-packages*
+  '(auctex
+    cider
+    clojure-mode
+    dash
+    epl
+    git-commit-mode
+    git-rebase-mode
+    magit
+    pkg-info))
+
+(defun archive-contents-file (archive-name)
+  (concat package-user-dir
+          "/archives"
+          "/" archive-name
+          "/archive-contents"))
+
+(defun ensure-package-archives ()
+  (unless (loop for (archive . address) in package-archives
+                always (file-exists-p (archive-contents-file archive)))
+    (package-refresh-contents)))
+
+(mapc (λ (package)
+        (unless (package-installed-p package)
+          (package-install package)))
+      *elpa-packages*)
+
+
+
 (custom-theme-set-variables
  'elpa-settings
-;'(package-enable-at-startup nil)
- '(package-archives (quote (("gnu"       . "http://elpa.gnu.org/packages/")
-                            ("marmalade" . "http://marmalade-repo.org/packages/")
-                            ("melpa"     . "http://melpa.milkbox.net/packages/")))))
+ '(package-enable-at-startup nil))
 
 (provide-theme 'elpa-settings)
