24ead9ad748df5b9b6fe4bc0e3b92220ec007527
Stuart Olsen <so1132@txstate.edu>
2015-04-10 02:28:06 -0500

    Highlighting for homework class

--- .emacs.d/themes/latex-settings-theme.el
+++ .emacs.d/themes/latex-settings-theme.el
@@ -3,6 +3,8 @@
 
 (custom-theme-set-variables
  'latex-settings
+ '(font-latex-match-sectioning-1-keywords (quote (("problem" "*{{"))))
+ '(font-latex-match-sectioning-2-keywords (quote (("subproblem" "*{{"))))
  '(TeX-PDF-mode t))
 
 (provide-theme 'latex-settings)
